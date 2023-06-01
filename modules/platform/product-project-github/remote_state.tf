c620fcb9d3e7f437120f0f3a6b26a502e8b4f1b0
Mike Verbanic <verbanicm@users.noreply.github.com>
2023-06-01 11:39:22 -0400

    create platform folder (#30)
    Using @gjonathanhong suggestion to co-locate the "platform" modules for
    better organization.

---
+++ modules/platform/product-project-github/remote_state.tf
@@ -0,0 +1,19 @@
+locals {
+  remote_state = {
+    for key, value in data.terraform_remote_state.remote_state : key => value.outputs
+  }
+}
+
+data "terraform_remote_state" "remote_state" {
+  for_each = {
+    org      = "${var.bucket_root_prefix}/org",
+    org_tags = "${var.bucket_root_prefix}/org/tags",
+  }
+
+  backend = "gcs"
+
+  config = {
+    bucket = var.bucket_name
+    prefix = each.value
+  }
+}
