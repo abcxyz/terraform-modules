b9962fb5155ea639de593b97d14f4039ccf6cbdc
Mike Verbanic <verbanicm@google.com>
2023-07-28 15:54:08 -0400

    chore: remove platform modules in favor of keeping them in repo

--- modules/platform/product-project-github/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "environments" {
-  value = {
-    for k, v in var.environments : k => {
-      project = module.projects.environments[k].project
-      wif     = module.github_wif[k]
-      storage = try(module.storage[k], null)
-    }
-  }
-}
--- modules/platform/product/modules/environment/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "folder" {
-  value = google_folder.default.name
-}
