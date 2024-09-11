3fa5b1c577aca53d3c6bb748d997d8e99df24405
Shan Kiyani <shankiyani@google.com>
2024-09-10 22:34:26 -0400

    Add cloud run alerts module

---
+++ examples/alerts_cloud_run/main.tf
@@ -0,0 +1,23 @@
+module "cloud_run_service_alerts" {
+  source = "git::https://github.com/abcxyz/terraform-modules.git//modules/alerts/cloud_run?ref=SHA_OR_TAG"
+
+  project_id = var.project_id
+
+  cloud_run_resource = {
+    service_name = "my-service-name"
+  }
+
+  notification_channels = ["notification-channel-id"]
+  runbook_urls = {
+    forward_progress = "https://github.com/org/repo/blob/main/docs/playbooks/alerts/ForwardProgressFailed.md"
+    cpu              = "https://github.com/org/repo/blob/main/docs/playbooks/alerts/IncreasedCPUUsage.md"
+  }
+
+  built_in_forward_progress_indicators = {
+    "request-count" = { metric = "request_count", window = 2 * local.hour + 10 * local.minute },
+  }
+
+  built_in_cpu_indicators = {
+    "cpu-utilization" = { metric = "utilization", window = 10 * local.minute, threshold : 0.8 },
+  }
+}
---
+++ modules/alerts_cloud_run/variables.tf
@@ -0,0 +1,65 @@
+# Copyright 2024 The Authors (see AUTHORS file)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+variable "project_id" {
+  description = "The GCP project ID."
+  type        = string
+}
+
+variable "cloud_run_resource" {
+  description = "One of either service name or job name which will dictate the Cloud Run resource to monitor."
+  type = object({
+    service_name = optional(string)
+    job_name     = optional(string)
+  })
+  validation {
+    condition     = length([for k, v in var.cloud_run_resource : v if v != null]) == 1
+    error_message = "Either service_name or job_name must be defined"
+  }
+}
+
+variable "runbook_urls" {
+  description = "URLs of markdown files."
+  type = object({
+    forward_progress = string
+    cpu              = string
+  })
+  default = {
+    forward_progress = ""
+    cpu              = ""
+  }
+}
+
+variable "built_in_forward_progress_indicators" {
+  description = "Map for forward progress Cloud Run indicators. The window must be in seconds."
+  type = map(object({
+    metric = string
+    window = number
+  }))
+}
+
+variable "built_in_cpu_indicators" {
+  description = "Map for CPU Cloud Run indicators. The window must be in seconds."
+  type = map(object({
+    metric    = string
+    window    = number
+    threshold = number
+  }))
+}
+
+variable "notification_channels" {
+  description = "List of notification channels to alert."
+  type        = list(string)
+  default     = []
+}
