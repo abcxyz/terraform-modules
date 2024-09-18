aee1c53304660fb21710b3640137d5232e69fb60
Stanley Chen <stanchen@google.com>
2024-09-18 23:22:39 +0000

    add status block

--- modules/cloud_run/outputs.tf
+++ modules/cloud_run/outputs.tf
@@ -27,7 +27,7 @@ output "url" {
   value       = google_cloud_run_service.service.status.0.url
 }
 
-output "latest_ready_revision_name" {
+output "revision_name" {
   description = "The Cloud Run latest revision name."
-  value       = google_cloud_run_service.service.latest_ready_revision_name
+  value       = google_cloud_run_service.service.status.0.latest_ready_revision_name
 }
