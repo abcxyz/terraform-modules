0fc2c3a6adcf2e6ac3ba424caa2f441ea517f037
Shan Kiyani <shankiyani@google.com>
2024-09-19 18:30:35 -0400

    Add new CR alert policies

--- examples/alerts_cloud_run/main.tf
+++ examples/alerts_cloud_run/main.tf
@@ -10,15 +10,33 @@ module "cloud_run_service_alerts" {
   notification_channels = ["notification-channel-id"]
   runbook_urls = {
     forward_progress = "https://github.com/org/repo/blob/main/docs/playbooks/alerts/ForwardProgressFailed.md"
-    cpu              = "https://github.com/org/repo/blob/main/docs/playbooks/alerts/IncreasedCPUUsage.md"
+    container_util   = "https://github.com/org/repo/blob/main/docs/playbooks/alerts/ContainerUtilization.md"
   }
 
   built_in_forward_progress_indicators = {
-    "request-count" = { metric = "request_count", window = 2 * local.hour + 10 * local.minute },
+    "request-count" = {
+      metric                        = "request_count"
+      window                        = 2 * local.hour + 10 * local.minute
+      threshold                     = 1
+      consecutive_window_violations = 1
+    },
   }
 
-  built_in_cpu_indicators = {
-    "cpu-utilization" = { metric = "utilization", window = 10 * local.minute, threshold : 0.8 },
+  built_in_container_util_indicators = {
+    "cpu" = {
+      metric                        = "container/cpu/utilizations"
+      window                        = 10 * local.minute
+      threshold                     = 0.8
+      p_value                       = 99
+      consecutive_window_violations = 1
+    },
+    "memory" = {
+      metric                        = "container/memory/utilizations"
+      window                        = 10 * local.minute
+      threshold                     = 0.8
+      p_value                       = 99
+      consecutive_window_violations = 1
+    },
   }
 
   log_based_text_indicators = {
@@ -26,14 +44,52 @@ module "cloud_run_service_alerts" {
       log_name_suffix      = "request"
       severity             = "ERROR"
       text_payload_message = "The request was aborted because there was no available instance."
+      condition_threshold = {
+        window                        = 10 * local.minute
+        threshold                     = 1
+        consecutive_window_violations = 1
+      }
     }
   }
 
   log_based_json_indicators = {
-    "image-resize-failure" = {
+    "email-bounce-failure" = {
       log_name_suffix      = "stdout"
       severity             = "ERROR"
-      json_payload_message = "Failed to resize image.*"
+      json_payload_message = "Failed.*"
+      additional_filters   = "jsonPayload.method=<your_method_name>"
     }
+  }
+
+  service_4xx_configuration = {
+    window                        = 300
+    threshold                     = 0
+    consecutive_window_violations = 1
+  }
+
+  service_5xx_configuration = {
+    window                        = 300
+    threshold                     = 0
+    consecutive_window_violations = 1
+  }
+
+  service_latency_configuration = {
+    window                        = 300
+    threshold                     = 0
+    consecutive_window_violations = 1
+    p_value                       = 95
+  }
+
+  service_max_conns_configuration = {
+    window                        = 300
+    threshold                     = 0
+    consecutive_window_violations = 1
+    p_value                       = 95
+  }
+
+  job_failure_configuration = {
+    window                        = 300
+    threshold                     = 0
+    consecutive_window_violations = 1
   }
 }
--- modules/alerts_cloud_run/variables.tf
+++ modules/alerts_cloud_run/variables.tf
@@ -32,30 +32,45 @@ variable "cloud_run_resource" {
 variable "runbook_urls" {
   description = "URLs of markdown files."
   type = object({
-    forward_progress = string
-    cpu              = string
+    forward_progress = optional(string)
+    container_util   = optional(string)
+    bad_request      = optional(string)
+    server_fault     = optional(string)
   })
   default = {
     forward_progress = ""
-    cpu              = ""
+    container_util   = ""
+    bad_request      = ""
+    server_fault     = ""
   }
 }
 
 variable "built_in_forward_progress_indicators" {
   description = "Map for forward progress Cloud Run indicators. The window must be in seconds."
   type = map(object({
-    metric = string
-    window = number
+    metric                        = string
+    window                        = number
+    threshold                     = number
+    consecutive_window_violations = number
   }))
 }
 
-variable "built_in_cpu_indicators" {
-  description = "Map for CPU Cloud Run indicators. The window must be in seconds."
+variable "built_in_container_util_indicators" {
+  description = "Map for Cloud Run container utilization indicators. The window must be in seconds. Threshold should be represented "
   type = map(object({
-    metric    = string
-    window    = number
-    threshold = number
+    metric                        = string
+    window                        = number
+    threshold                     = number
+    p_value                       = number
+    consecutive_window_violations = number
   }))
+  validation {
+    condition = alltrue([
+      for k, v in var.built_in_container_util_indicators :
+      contains([50, 95, 99], v.p_value)
+    ])
+    error_message = "The 'p_value' field must be one of: 50, 95, 99."
+  }
 }
 
 variable "log_based_text_indicators" {
@@ -65,6 +80,11 @@ variable "log_based_text_indicators" {
     severity             = string
     text_payload_message = string
     additional_filters   = optional(string)
+    condition_threshold = optional(object({
+      window                        = number
+      threshold                     = number
+      consecutive_window_violations = number
+    }))
   }))
   validation {
     condition = alltrue([
@@ -83,6 +103,11 @@ variable "log_based_json_indicators" {
     severity             = string
     json_payload_message = string
     additional_filters   = optional(string)
+    condition_threshold = optional(object({
+      window                        = number
+      threshold                     = number
+      consecutive_window_violations = number
+    }))
   }))
   validation {
     condition = alltrue([
@@ -98,4 +123,86 @@ variable "notification_channels" {
   description = "List of notification channels to alert."
   type        = list(string)
   default     = []
+}
+
+variable "service_4xx_configuration" {
+  description = "Configuration applied to the 4xx alert policy. Only applies to services."
+  type = object({
+    window                        = number
+    threshold                     = number
+    consecutive_window_violations = number
+  })
+  default = {
+    window                        = 300
+    threshold                     = 0
+    consecutive_window_violations = 2
+  }
+}
+
+variable "service_5xx_configuration" {
+  description = "Configuration applied to the 5xx alert policy. Only applies to services."
+  type = object({
+    window                        = number
+    threshold                     = number
+    consecutive_window_violations = number
+  })
+  default = {
+    window                        = 300
+    threshold                     = 0
+    consecutive_window_violations = 2
+  }
+}
+
+variable "service_latency_configuration" {
+  description = "Configuration applied to the request latency alert policy. Only applies to services."
+  type = object({
+    window                        = number
+    threshold                     = number
+    consecutive_window_violations = number
+    p_value                       = number
+  })
+  default = {
+    window                        = 300
+    threshold                     = 0
+    consecutive_window_violations = 2
+    p_value                       = 95
+  }
+  validation {
+    condition     = contains([50, 95, 99], var.service_latency_configuration.p_value)
+    error_message = "The 'p_value' field must be one of: 50, 95, 99."
+  }
+}
+
+variable "service_max_conns_configuration" {
+  description = "Configuration applied to the max connections alert policy. Only applies to services."
+  type = object({
+    window                        = number
+    threshold                     = number
+    consecutive_window_violations = number
+    p_value                       = number
+  })
+  default = {
+    window                        = 300
+    threshold                     = 0
+    consecutive_window_violations = 2
+    p_value                       = 95
+  }
+  validation {
+    condition     = contains([50, 95, 99], var.service_max_conns_configuration.p_value)
+    error_message = "The 'p_value' field must be one of: 50, 95, 99."
+  }
+}
+
+variable "job_failure_configuration" {
+  description = "Configuration applied to the job failure alert policy. Only applies to jobs."
+  type = object({
+    window                        = number
+    threshold                     = number
+    consecutive_window_violations = number
+  })
+  default = {
+    window                        = 300
+    threshold                     = 0
+    consecutive_window_violations = 1
+  }
 }
